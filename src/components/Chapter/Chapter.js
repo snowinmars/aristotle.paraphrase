66b292a3b03d68fff0b0806fffe66a1f9732ab63
snowinmars <marcor@yandex.ru>
2020-01-18 20:31:57 +0300

    Cleaned up this mess a bit

--- src/components/Chapter/Chapter.js
+++ src/components/Chapter/Chapter.js
@@ -7,39 +7,27 @@ import { highlight_paragraph }  from './../../actions/index';
 import { connect } from 'react-redux'
 
 class Chapter extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    settings = {
-        
-    };
-    
-    render_paragraph = (paragraph, index) => {
-        const highlight = ({children}) => (
-            <span className="highlighted-term">{children}</span>
-        );
-        
-        let tags = null;
+    settings = {};
+
+    parse_tags = (paragraph) => {
         let highlighted_paragraphs = [];
         let text = paragraph;
-        
+
         if (paragraph.startsWith('[')) {
             const items = paragraph.split('\n');
             text = items.pop();
-            tags = items;
 
-            tags.map(tag => {
+            items.map(tag => {
                 if (!(tag.startsWith('[') && tag.endsWith(']'))) {
                     throw `Wrong tag format: ${tag}`;
                 }
-                
+
                 const [name, value] = tag.slice(1).slice(0, -1).split(':');
 
                 if (name !== 'ref') {
                     throw `Wrong tag name: ${name}`;
                 }
-                
+
                 if (+value <= 0) {
                     throw `Wrong tag value: ${value}`
                 }
@@ -54,42 +42,58 @@ class Chapter extends React.Component {
             })
         }
 
-        const raise_paragraph_highlight = (tags) => {
-            if (!tags || tags.length  === 0) {
-                this.props.set_hovered_paragraph({});
-                return;
-            }
+        return {
+            text, highlighted_paragraphs
+        }
+    };
 
-            const settings = {
-                from: {
-                    destination: this.settings.destination,
-                    index: index,
-                },
-                to: highlighted_paragraphs
-            };
-            
-            this.props.set_hovered_paragraph(settings);
+    raise_paragraph_highlight = (highlighted_paragraphs, index) => {
+        if (!highlighted_paragraphs) {
+            this.props.set_hovered_paragraph({});
+            return;
+        }
+
+        const settings = {
+            from: {
+                destination: this.settings.destination,
+                index: index,
+            },
+            to: highlighted_paragraphs
         };
-        
+
+        this.props.set_hovered_paragraph(settings);
+    };
+
+
+    render_paragraph = (paragraph, index) => {
+        const highlight = ({children}) => (
+            <span className="highlighted-term">{children}</span>
+        );
+
+        const parsed = this.parse_tags(paragraph);
+
+        const isInternalParagraph = this.props.from && this.props.from.destination === this.settings.destination && this.props.from.index === index;
+
+        const to = this.props.to && this.props.to.find(x => x.destination === this.settings.destination && x.index === index);
+        const isExternalParagraph = to && to.destination === this.settings.destination && to.index === index;
+
         let className = 'unhighlighted-paragraph';
-        const asd = this.props.to && this.props.to.find(x => x.destination === this.settings.destination && x.index === index)
-        
-        if ((asd && asd.destination === this.settings.destination && asd.index === index) ||
-            (this.props.from && this.props.from.destination === this.settings.destination && this.props.from.index === index)) {
+
+        if (isInternalParagraph || isExternalParagraph) {
             className = 'highlighted-paragraph'
         }
-        
+
         return (
             <p className={'paragraph'} key={`${this.props.item.id}_${index}`}>
                 <Highlighter
                     searchWords={terms}
-                    textToHighlight={text}
+                    textToHighlight={parsed.text}
                     className={className}
                     autoEscape={true}
                     highlightTag={highlight}
                     dangerouslySetInnerHTML={true}
-                    onMouseEnter={() => raise_paragraph_highlight(tags)}
-                    onMouseLeave={() => raise_paragraph_highlight(null)}
+                    onMouseEnter={() => this.raise_paragraph_highlight(parsed.highlighted_paragraphs, index)}
+                    onMouseLeave={() => this.raise_paragraph_highlight(null)}
                 />
             </p>
         );
@@ -99,16 +103,16 @@ class Chapter extends React.Component {
         const [destination, raw_book, raw_chapter] = id.split('_');
         const book = raw_book.slice(1);
         const chapter = raw_chapter.slice(1);
-        
+
         this.settings.destination = destination;
         this.settings.book = book;
         this.settings.chapter = chapter;
     };
-    
+
     render = () => {
         const chapter = this.props.item;
         this.parse_id(chapter.id);
-        
+
         return (
             <div className={'chapter'} key={chapter.id}>
                 <div className={'chapter-title'}>
