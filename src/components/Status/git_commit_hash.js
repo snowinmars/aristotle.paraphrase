4443c76a774a864f43ccb85b73adcdae7865fa62
snowinmars <marcor@yandex.ru>
2020-02-02 02:03:26 +0300

    Fix bug with paraphrase note ids

--- src/components/Status/git_commit_hash.js
+++ src/components/Status/git_commit_hash.js
@@ -1,1 +1,1 @@
-const git_commit_hash = "ceb301adc25d7961fac7a40dff4c093807a47e09"; export default git_commit_hash;
+const git_commit_hash = "851f33dd3a236add59d9a83330d1341db4ef4c8c"; export default git_commit_hash;
