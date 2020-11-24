8ccbe4f670f1b68d50cb192146634b7e8a828adf
snowinmars <snowinmars@yandex.ru>
2020-11-24 22:12:57 +0300

    Rewrite c1-c6

--- src/components/Paraphrase/b1_notes/b1_c5.notes.js
+++ src/components/Paraphrase/b1_notes/b1_c5.notes.js
@@ -4,11 +4,11 @@ const notes = `
 <sup>(п1)</sup> Здесь и несколько глав далее Аристотель активно описывает взгляды современников на физику. Я сокращу такие части: детальные взгляды греческих философов на физику уже не актуальны и, признаться, не особо понятны.
 
 [ref:paraphrase 0]
-<sup>(п1)</sup> Есть хороший пример философии Пифагора в современном кино: фильм "Пи" (1997). <br /> <iframe width="560" height="315" src="https://www.youtube.com/embed/TxCt093lDWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<sup>(п2)</sup> Есть хороший пример философии Пифагора в современном кино: фильм "Пи" (1997). <br /> <iframe width="560" height="315" src="https://www.youtube.com/embed/TxCt093lDWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 [ref:paraphrase 3]
 [ref:paraphrase 6]
-<sup>(п3)</sup> Ранее по тексту термин "единое" не разъясняется. О нём будет много сказано в пятой книге. Пока можно считать "единое" самой высшей сущностью.
+<sup>(п3)</sup> Ранее по тексту термин "единое" не разъясняется. О нём будет много сказано в пятой книге. Пока можно считать "единое" самой высшей сущностью - богом?
 
 `;
 
