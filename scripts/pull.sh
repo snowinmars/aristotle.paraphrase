41f1c4ccd38baf24de94847794931b620fde1bbd
snowinmars <snowinmars@yandex.ru>
2021-09-27 00:28:47 +0300

    Drop unused scripts

--- scripts/pull.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker pull snowinmars/prf-fe
-docker pull snowinmars/prf-be
-docker pull snowinmars/prf-ngx
--- scripts/push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Push last builded local images..."
-
-docker push snowinmars/prf-fe
-docker push snowinmars/prf-be
-docker push snowinmars/prf-ngx
